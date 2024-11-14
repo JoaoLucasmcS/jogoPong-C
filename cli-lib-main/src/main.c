b5890a4bcc7f2e8191e73ec9d8e1a57531a820a7
João Lucas <163215635+JoaoLucasmcS@users.noreply.github.com>
2024-11-14 18:31:52 -0300

    feat jogo_Pong

--- cli-lib-main/src/main.c
+++ cli-lib-main/src/main.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
-#include "screen.h"
-#include "keyboard.h"
-#include "timer.h"
+#include "../include/screen.h"
+#include "../include/keyboard.h"
+#include "../include/timer.h"
 
-// Variáveis globais do jogo
+#define TAMANHO_RAQUETE 1
+
 int pontoJogador1 = 0, pontoJogador2 = 0;
 int posicaoXRaquete1 = 5, posicaoYRaquete1 = MAXY / 2;
 int posicaoXRaquete2 = MAXX - 5, posicaoYRaquete2 = MAXY / 2;
 int x = MAXX / 2, y = MAXY / 2;
 int incX = 1, incY = 1;
 
-// Funções auxiliares
+void inicializarJogo() {
+    screenInit(1);
+    keyboardInit();
+    timerInit(90);
+}
+
+void resetarBolinha() {
+    x = MAXX / 2;
+    y = MAXY / 2;
+    incX = -incX;
+}
+
 void mostrarBolinha(int nextX, int nextY) {
-    screenSetColor(WHITE, WHITE);
+    screenSetColor(DARKGRAY, DARKGRAY);
     screenGotoxy(x, y);
     printf(" ");
+
     x = nextX;
     y = nextY;
+
+    screenSetColor(WHITE, DARKGRAY);
     screenGotoxy(x, y);
     printf("O");
 }
@@ -29,14 +44,16 @@ void mostrarPlacar() {
     printf("Jogador 2: %d", pontoJogador2);
 }
 
-void raquete(int xRaquete, int yRaquete, int newXRaquete, int newYRaquete) {
-    screenSetColor(BLUE, BLUE);
-    for (int i = -1; i <= 1; i++) {
+void desenharRaquete(int xRaquete, int yRaquete, int tamanhoRaquete) {
+    screenSetColor(DARKGRAY, DARKGRAY);
+    for (int i = -tamanhoRaquete; i <= tamanhoRaquete; i++) {
         screenGotoxy(xRaquete, yRaquete + i);
         printf(" ");
     }
-    for (int i = -1; i <= 1; i++) {
-        screenGotoxy(newXRaquete, newYRaquete + i);
+
+    screenSetColor(BLUE, DARKGRAY);
+    for (int i = -tamanhoRaquete; i <= tamanhoRaquete; i++) {
+        screenGotoxy(xRaquete, yRaquete + i);
         printf("|");
     }
 }
@@ -44,33 +61,57 @@ void raquete(int xRaquete, int yRaquete, int newXRaquete, int newYRaquete) {
 void calcularPlacar() {
     if (x >= MAXX - 1) {
         pontoJogador1++;
-        x = MAXX / 2; y = MAXY / 2;
+        resetarBolinha();
+        screenDrawBorders();
     } else if (x <= 1) {
         pontoJogador2++;
-        x = MAXX / 2; y = MAXY / 2;
+        resetarBolinha();
+        screenDrawBorders();
     }
 }
 
 void moverRaquetes(int ch) {
     if (ch == 'w' && posicaoYRaquete1 > MINY + 1) posicaoYRaquete1--;
     if (ch == 's' && posicaoYRaquete1 < MAXY - 2) posicaoYRaquete1++;
+
     if (ch == 'i' && posicaoYRaquete2 > MINY + 1) posicaoYRaquete2--;
     if (ch == 'k' && posicaoYRaquete2 < MAXY - 2) posicaoYRaquete2++;
 }
 
-int colisaoRaquete() {
-    if (x == posicaoXRaquete1 + 1 && y >= posicaoYRaquete1 - 1 && y <= posicaoYRaquete1 + 1) return -1;
-    if (x == posicaoXRaquete2 - 1 && y >= posicaoYRaquete2 - 1 && y <= posicaoYRaquete2 + 1) return -1;
-    return 1;
+int verificarColisaoRaquete() {
+    if (x == posicaoXRaquete1 + 1 && y >= posicaoYRaquete1 - TAMANHO_RAQUETE && y <= posicaoYRaquete1 + TAMANHO_RAQUETE) {
+        return -incX;
+    }
+
+    if (x == posicaoXRaquete2 - 1 && y >= posicaoYRaquete2 - TAMANHO_RAQUETE && y <= posicaoYRaquete2 + TAMANHO_RAQUETE) {
+        return -incX;
+    }
+
+    return incX;
+}
+
+void limparRaquetes() {
+    screenGotoxy(posicaoXRaquete1, posicaoYRaquete1);
+    printf(" ");
+    screenGotoxy(posicaoXRaquete1, posicaoYRaquete1 + 1);
+    printf(" ");
+    screenGotoxy(posicaoXRaquete1, posicaoYRaquete1 - 1);
+    printf(" ");
+
+    screenGotoxy(posicaoXRaquete2, posicaoYRaquete2);
+    printf(" ");
+    screenGotoxy(posicaoXRaquete2, posicaoYRaquete2 + 1);
+    printf(" ");
+    screenGotoxy(posicaoXRaquete2, posicaoYRaquete2 - 1);
+    printf(" ");
+    
 }
 
 int main() {
     int ch = 0;
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
+    inicializarJogo();
 
-    while (ch != 10) { // Pressione Enter para sair
+    while (ch != 10) {
         if (keyhit()) {
             ch = readch();
             moverRaquetes(ch);
@@ -80,21 +121,22 @@ int main() {
             int newX = x + incX;
             int newY = y + incY;
 
-            // Colisão com bordas e raquetes
-            if (newY >= MAXY - 1 || newY <= MINY + 1) incY = -incY;
-            incX *= colisaoRaquete();
+            if (newY >= MAXY - 1 || newY <= MINY + 1) {
+                incY = -incY;
+            }
 
-            // Movimentos da bola e raquetes
+            incX = verificarColisaoRaquete();
+
             mostrarBolinha(newX, newY);
-            raquete(posicaoXRaquete1, posicaoYRaquete1, posicaoXRaquete1, posicaoYRaquete1);
-            raquete(posicaoXRaquete2, posicaoYRaquete2, posicaoXRaquete2, posicaoYRaquete2);
+            desenharRaquete(posicaoXRaquete1, posicaoYRaquete1, TAMANHO_RAQUETE);
+            desenharRaquete(posicaoXRaquete2, posicaoYRaquete2, TAMANHO_RAQUETE);
             calcularPlacar();
             mostrarPlacar();
             screenUpdate();
+            limparRaquetes();
         }
     }
 
-    // Encerrar o jogo e limpar a tela
     keyboardDestroy();
     screenDestroy();
     timerDestroy();
